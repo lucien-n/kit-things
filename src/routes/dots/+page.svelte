c65d5b3c7f403537c98d48e818948adc1b350302
Lucien <lucien.neuhoff@protonmail.com>
2024-04-11 15:30:29 +0200

    wip

--- src/routes/dots/+page.svelte
+++ src/routes/dots/+page.svelte
@@ -1,6 +1,9 @@
 <script lang="ts">
 	import { onMount } from 'svelte';
-	import { Label } from '$shadcn/ui/ui';
+	import { Label } from '$shadcn/ui/label';
+	import { Checkbox } from '$shadcn/ui/checkbox';
+	import { Input } from '$shadcn/ui/input';
+	import { Card } from '$shadcn/ui/card';
 
 	type Dot = {
 		startX: number;
@@ -17,28 +20,28 @@
 	let width = 0;
 	let height = 0;
 
-	const velocity = 5 / 1000;
-	let prevTime = 0;
-	let dt = $state(0);
-
-	let maxDistanceFromStart = $state(10);
+	let spacing = $state(50);
+	let velocity = $state(0.005);
+	let velocityCap = $state(0.3);
+	let maxDistanceFromStart = $state(25);
 	let clear = $state(true);
-
-	const now = () => new Date().getTime();
+	let drawDebug = $state(false);
 
 	const generateDots = (): Dot[] => {
 		const dots: Dot[] = [];
-		const spacing = 50;
 
 		for (let y = 0; y < height / spacing; y++) {
 			for (let x = 0; x < width / spacing; x++) {
+				const dotX = x * spacing + spacing / 2;
+				const dotY = y * spacing + spacing / 2;
+
 				dots.push({
-					x: x * spacing,
-					y: y * spacing,
+					x: dotX,
+					y: dotY,
 					velX: 0,
 					velY: 0,
-					startX: x * spacing,
-					startY: y * spacing
+					startX: dotX,
+					startY: dotY
 				});
 			}
 		}
@@ -67,40 +70,59 @@
 			ctx.clearRect(0, 0, width, height);
 		}
 
-		dt = now() - prevTime;
-		prevTime = now();
-
 		for (const dot of dots) {
-			const vel = velocity * dt;
-			dot.velY += Math.round(Math.random()) ? -vel : vel;
-			dot.velX += Math.round(Math.random()) ? -vel : vel;
+			dot.velY += Math.round(Math.random()) ? -velocity : velocity;
+			dot.velX += Math.round(Math.random()) ? -velocity : velocity;
 
-			if (dot.velY > 0.5) dot.velY = 0.5;
-			if (dot.velY < -0.5) dot.velY = -0.5;
-			if (dot.velX > 0.5) dot.velX = 0.5;
-			if (dot.velX < -0.5) dot.velX = -0.5;
+			if (dot.velY > velocityCap) dot.velY = velocityCap;
+			if (dot.velY < -velocityCap) dot.velY = -velocityCap;
+			if (dot.velX > velocityCap) dot.velX = velocityCap;
+			if (dot.velX < -velocityCap) dot.velX = -velocityCap;
 
-			if (dot.x < 0 || dot.x > width) {
-				dot.velX = -dot.velX;
-			} else {
-				const offsetX = dot.x - dot.startX;
-				if (offsetX > maxDistanceFromStart || offsetX < -maxDistanceFromStart) dot.velX = -dot.velX;
-			}
-
-			if (dot.y < 0 || dot.y > width) {
-				dot.velY = -dot.velY;
-			} else {
-				const offsetY = dot.y - dot.startY;
-				if (offsetY > maxDistanceFromStart || offsetY < -maxDistanceFromStart) dot.velY = -dot.velY;
+			const distanceFromStart = Math.sqrt((dot.x - dot.startX) ** 2 + (dot.y - dot.startY) ** 2);
+			if (distanceFromStart > maxDistanceFromStart) {
+				const angle = Math.atan2(dot.y - dot.startY, dot.x - dot.startX);
+				dot.velX = -Math.cos(angle) * velocity;
+				dot.velY = -Math.sin(angle) * velocity;
 			}
 
 			dot.x += dot.velX;
 			dot.y += dot.velY;
+
+			ctx.fillStyle = 'black';
+			ctx.fillRect(dot.x, dot.y, 3, 3);
+
+			if (drawDebug) {
+				ctx.fillStyle = 'rgba(255, 0, 0, .3)';
+				ctx.fillRect(dot.startX - 2, dot.startY - 2, 5, 5);
+
+				ctx.translate(dot.startX, dot.startY);
+				ctx.beginPath();
+				ctx.arc(0, 0, maxDistanceFromStart, 0, Math.PI * 2);
+				ctx.strokeStyle = 'rgba(0, 0, 0, .3)';
+				ctx.stroke();
+				ctx.setTransform(1, 0, 0, 1, 0, 0);
+			}
 		}
 
-		ctx.strokeStyle = 'black';
 		for (const dot of dots) {
-			ctx.fillRect(dot.x, dot.y, 3, 3);
+			dot.velY += Math.round(Math.random()) ? -velocity : velocity;
+			dot.velX += Math.round(Math.random()) ? -velocity : velocity;
+
+			if (dot.velY > velocityCap) dot.velY = velocityCap;
+			if (dot.velY < -velocityCap) dot.velY = -velocityCap;
+			if (dot.velX > velocityCap) dot.velX = velocityCap;
+			if (dot.velX < -velocityCap) dot.velX = -velocityCap;
+
+			const distanceFromStart = Math.sqrt((dot.x - dot.startX) ** 2 + (dot.y - dot.startY) ** 2);
+			if (distanceFromStart > maxDistanceFromStart) {
+				const angle = Math.atan2(dot.y - dot.startY, dot.x - dot.startX);
+				dot.velX = -Math.cos(angle) * velocity;
+				dot.velY = -Math.sin(angle) * velocity;
+			}
+
+			dot.x += dot.velX;
+			dot.y += dot.velY;
 		}
 
 		requestAnimationFrame(update);
@@ -121,30 +143,31 @@
 
 <svelte:window onresize={handleResize} />
 <div class="absolute left-0 top-0 p-3">
-	<div class="bg-neutral-600 bg-opacity-80 px-3 py-2 text-white">
-		<p><strong>DT</strong> {dt}</p>
-		<section>
-			<Label />
-			<label for="clearCB">
-				<strong>CLEAR</strong>
-			</label>
-			<input type="checkbox" name="clear" id="clearCB" bind:checked={clear} />
-		</section>
-		<section>
-			<label for="maxDistanceINP">
-				<strong>MAX DISTANCE</strong>
-			</label>
-			<input
-				type="number"
-				name="maxDistance"
-				id="maxDistanceINP"
-				min="0"
-				max="500"
-				class="text-black"
-				bind:value={maxDistanceFromStart}
-			/>
-		</section>
-	</div>
+	<Card class="grid grid-cols-2 items-center gap-4 px-3 py-2">
+		<Label for="clearCheckbox" class="font-bold">CLEAR</Label>
+		<Checkbox id="clearCheckbox" bind:checked={clear} />
+
+		<Label for="drawDebugCheckbox" class="font-bold">DRAW DEBUG</Label>
+		<Checkbox id="drawDebugCheckbox" bind:checked={drawDebug} />
+
+		<Label for="maxDistanceInput" class="font-bold">MAX DISTANCE</Label>
+		<Input
+			id="maxDistanceInput"
+			type="number"
+			min="0"
+			max="500"
+			bind:value={maxDistanceFromStart}
+		/>
+
+		<Label for="velocityInput" class="font-bold">VELOCITY</Label>
+		<Input id="velocityInput" type="number" bind:value={velocity} />
+
+		<Label for="velocityCapInput" class="font-bold">VELOCITY CAP</Label>
+		<Input id="velocityCapInput" type="number" bind:value={velocityCap} />
+
+		<Label for="spacingInput" class="font-bold">SPACING</Label>
+		<Input id="spacingInput" type="number" bind:value={spacing} on:change={handleResize} />
+	</Card>
 </div>
 
 <canvas bind:this={canvas} class="h-full w-full" />
